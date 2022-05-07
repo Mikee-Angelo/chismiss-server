f62329e0186a5f6a18b42a5255598d7a8fc7454c
Michael Angelo De Leon <iammikedev@gmail.com>
2022-05-07 22:01:08 +0800

    implemented firebase admin

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express') 
 const bodyParser = require('body-parser')
-const { initializeApp } = require('firebase-admin/app')
+const { applicationDefault, initializeApp } = require('firebase-admin/app')
+const { getAuth } = require('firebase-admin/auth')
 const app = express() 
 const port = 3000
 
-app.use(express.json()) 
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
+initializeApp({ 
+    credential: applicationDefault()
+})
 
+const allUsers = async (nextPageToken) => { 
+
+    let result = await getAuth().listUsers(1000, nextPageToken); 
+
+    return result.users; 
+}
+
+app.get('/', async (req, res) => {
+    let users = await allUsers()
     
     res.send({
-        message: 'Hello World'
+        'data': users
     })
 }) 
 
